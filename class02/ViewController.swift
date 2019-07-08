b663e30ce263543d564e1cf51428553bdb7ad171
01 <>
2019-07-08 17:10:47 +0800

    022

--- class02/ViewController.swift
+++ class02/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbMSG: UILabel!
+    var result:float_t=10.2
+    
+    
+    @IBAction func btnTestClicked(_ sender: Any) {
+  print("我按了什么")
+ 
+        lbMSG.text="我要铜锣烧"
+    lbMSG.textColor=UIColor.red
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let 如果我去超市=true
+        let 如果看见🍊=true
+        if(如果我去超市){
+            var 买几个🍉=1
+            if(如果看见🍊){
+                买几个🍉=10
+            }
+               print("买\(买几个🍉)个🍉")
+        }
+        
+        let de = add(a: 1, b: 8)
+        
+        var ce:Int=1
+        
+        print(de)
+        print("我一定学的会 Swift")
+   
+    
     }
 
-
+    func add(a :Int,b :Int) -> Int {
+return a + b
+    }
 }
 
