6ad50d3390a1b69bed23d8f1e5c3cf7fc7672c3e
01 <>
2019-07-09 17:01:38 +0800

    006

--- class02/AViewController.swift
+++ class02/AViewController.swift
@@ -10,29 +10,47 @@ import UIKit
 
 class AViewController: UIViewController {
     
-    @IBOutlet weak var lbMMSG: UITextField!
+    @IBOutlet weak var lbXLMsg: UITextField!
     
-    @IBAction func btnTestClicked(_ sender: Any) {
-        print("我按了什么")
-        
-        lbMMSG.text="我要吃大西瓜"
-        lbMMSG.textColor=UIColor.red
-    }
+    @IBOutlet weak var btnHibottomconstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    @IBAction func btnACllicked(_ sender: UIButton)
+    {
+        print("我按了啥?")
+        
+        lbXLMsg.text = "我要 芒果"
+        lbXLMsg.textColor = UIColor.red
+   }
+    
+  
+    
+    
+    //.tentcent.com
+    @objc func keyboardWillShow(notification: NSNotification) {
+        let keyboardHeight = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.height
+        print("猜猜看：\(keyboardHeight)")
+         btnHibottomconstraint.constant=400
     }
     
-
+    
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
